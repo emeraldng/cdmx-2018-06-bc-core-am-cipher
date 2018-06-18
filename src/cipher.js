84c04021ec74bf7488e582e403c83fe1647c3f8e
emeraldng <ifglitterthen@gmail.com>
2018-06-18 10:10:12 -0500

    cambio de js

--- src/cipher.js
+++ src/cipher.js
@@ -1,27 +1,60 @@
 
-  //función para encriptar 
-  const encriptar = (string, offset) => { 
-  let resultado = ""; 
-  let encriptar = resultado; 
-  //estableciendo los límites para las mayúsculas y minusculas. 
-  for (let i=0; i<string.lenght; i++){ 
-  //para mayusculas 
-  if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90) { 
-  code=(((string.charCodeAt(i)-65)+offset)%26)+65; 
-  } 
-  //para minusculas 
-  else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){ 
-  code= (((string.charCodeAt(i)-97)+offset)%26)+97; 
-  } 
-  else if (string.charCodeAt(i)===32) 
-  code = 32; 
-  } 
-  return resultado; 
-  } 
-  //función para imprimir el resultado en un lugar en especifico 
-  const joven =() => { 
-  let jovencillo = document.getElementById("txt1").value; 
-  document.getElementById("cajilla").innerHTML=(encriptar(jovencillo, 15)); 
+function cajasParaDepositarDatos() {
+  document.getElementById('decipher').value = "";
+  document.getElemntbyId('cipher').value = "";
+  document.getElementById("result").value = "";
+  document.getElementById("offset").value = 1;
   }
-  
+
+  function cipher() {
+      let decipher =
+      document.getElementById('decipher').value.toLocaleUpperCase() || "";
+      let cipher = document.getElementById('cipher');
+      let offset = document.getElementById('offset');
+      let cipherArray = '';
+      [...decipher].forEach(char => {
+      //console.log(char.charCodeAt())
+      if (char.charCodeAt() === 32) {
+      cipherArray.push(32);
+      } else {
+      charNumber = char.charCodeAt() - 65;
+      //console.log(charNumber)
+      cipherChar = (charNumber + parseInt(offset.value)) % 26;
+      //console.log(cipherChar)
+      cipherArray.push(cipherChar + 65);
+      }
+      })
+      document.getElementById("result").value =
+      String.fromCharCode(...cipherArray);
+      //console.log(String.fromCharCode(...cipherArray))
+      }
+      
+      
+      function decipher() {
+      let decipher = document.getElementById('decipher');
+      let cipher =
+      document.getElementById('cipher').value.toLocaleUpperCase();
+      let offset = document.getElementById('offset');
+      let cipherArray = new Array();
+      [...encrypted].forEach(char => {
+      //console.log(char.charCodeAt())
+      if (char.charCodeAt() === 32) {
+      cipherArray.push(32);
+      } else {
+      charNumber = char.charCodeAt() - 65;
+      //console.log(charNumber)
+      cipherChar = (charNumber - parseInt(offset.value));
+      //console.log(cipherChar)
+      if (cipherChar > 25 || cipherChar < 0) cipherChar =
+      cipherChar + 26
+      cipherChar = cipherChar % 26
+      cipherArray.push(cipherChar + 65);
+      }
+      })
+        
+      document.getElementById("result").value =
+      String.fromCharCode(...cipherArray);
+      //console.log(String.fromCharCode(...cipherArray))
+      }
+      
   
