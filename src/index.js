a92c2da4351cd63ef7e231b464ad8c3328148088
emeraldng <ifglitterthen@gmail.com>
2018-06-18 09:37:37 -0500

    cambio de js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+ //Guardando los datos del usuario en una variable. 
+ function sss(){ 
+    //para obtener el texto 
+    let mensaje = document.getElementById("txt1").value; 
+    document.getElementById("aaa").innerHTML=("La palabra encriptada es"+ mensaje); 
+    //para obtener el nº de posiciones a mover 
+    let posicion = document.getElementById("psw1").value; 
+    document.getElementById("bbb").innerHTML=("La palabra encriptada es" + posicion); 
+    } 
