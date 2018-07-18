f8a18255e3c225ae27928df03255ea4e235ded85
emeraldng <ifglitterthen@gmail.com>
2018-07-18 12:36:27 -0500

    Las funciones ya codifican y decodifican

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,5 @@
+
+
 // const strFromUser = document.getElementById("textBox1").value;
 // const strFromUser2 = document.getElementById("textBox2").value;
 // let offset = parseInt(document.getElementById("pass1").value);
@@ -7,23 +9,30 @@ window.cipher = {
     let txTCifradoInArray = [];
     let result2Encode = "";
     let encodTxt = result2Encode;
-    
+    let offsetFunction = offset;
+    console.log(offsetFunction)    
     for (let i = 0; i < string.length; i++) {
       var code = string.charCodeAt(i);
       txtInArrayWithAsciiCode.push(code);
+      console.log(txtInArrayWithAsciiCode);
     }
-    let newArraywithEncode = txtInArrayWithAsciiCode.map((v, i, a) => {
+    let newArraywithEncode = txtInArrayWithAsciiCode.map((v) => {
+      console.log(v)
       if (v >= 65 && v <= 90) {
         return (((v - 65) + offset) % 26) + 65;
       } else if (v >= 97 && v <= 122) {
+        console.log((((v - 97) + offset) % 26) + 97)
         return (((v - 97) + offset) % 26) + 97;
       } else {
         return v = 32;
       }
-      result2Encode = String.fromCharCode.apply(String, newArraywithEncode);
-      
+      //console.log(newArraywithEncode);
+
     })
-    return result2Encode;
+     //String.fromCharCode.apply(String, newArraywithDecode);
+    return String.fromCharCode.apply(String, newArraywithEncode);
+    //return newArraywithEncode.join("");
+    
     
 
   },
@@ -46,10 +55,10 @@ window.cipher = {
       }else{
         return v=32;
       }
-      result2Decode = String.fromCharCode.apply(String, newArraywithDecode);
+      //result2Decode = String.fromCharCode.apply(String, newArraywithDecode);
       
     })
-    return result2Decode;
+    return String.fromCharCode.apply(String, newArraywithDecode);
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const button1 = document.getElementById("buttonEn");
 const button2 = document.getElementById("buttonDe");
 //let string = document.getElementById("textBox1").value;
-//let offset = parseInt(document.getElementById("pass1").value);
+let offset = parseInt(document.getElementById("pass1").value);
 
 buttonEn.addEventListener("click", event => {
   let string = document.getElementById("textBox1").value;
   let offset = parseInt(document.getElementById("pass1").value);
-  let result2Encode = cipher.encode(offset, strFromUser);
+  let result2Encode = cipher.encode(offset, string);
   document.getElementById("encodetxt").innerHTML = result2Encode;
 });
 button2.addEventListener("click", event => {
   let string = document.getElementById("textBox2").value;
   let offset = parseInt(document.getElementById("pass1").value);
-  let result2Decode = cipher.decode(offset, strFromUser2);
+  let result2Decode = cipher.decode(offset, string);
   document.getElementById("decodetxt").innerHTML = result2Decode;
   
 });
