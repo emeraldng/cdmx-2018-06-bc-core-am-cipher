f06534d3d9b63c29250af4530846c19106b0d3f5
emeraldng <ifglitterthen@gmail.com>
2018-07-23 01:47:05 -0500

    Añadiendo comentarios de funciones declaradas

--- src/cipher.js
+++ src/cipher.js
@@ -1,63 +1,90 @@
-
-
-// const strFromUser = document.getElementById("textBox1").value;
-// const strFromUser2 = document.getElementById("textBox2").value;
-// let offset = parseInt(document.getElementById("pass1").value);
 window.cipher = {
   encode: (offset, string) => {
+    
+    //Declaro un array vacío, donde se vertirá los nuevos códigos ASCII ya con offset rotado.
     let txtInArrayWithAsciiCode = [];
-    let txTCifradoInArray = [];
-    let result2Encode = "";
-    let encodTxt = result2Encode;
+    //Declaré el offset en un var, para poder ver si el valor del input estaba siendo recibido.
     let offsetFunction = offset;
-    console.log(offsetFunction)    
+    //Verificando que el offset se está tomando.
+    console.log(offsetFunction) 
+    
+    //Iniciando un loop para recorrer el string dado por el usuario.
     for (let i = 0; i < string.length; i++) {
-      var code = string.charCodeAt(i);
+      //Obtenemos a traves del loop el código ASCII de cada caracter(letra) del string.
+      //Lo guardamos en la variable: "code".
+      let code = string.charCodeAt(i);
+      //Los códigos ASCII obtenidos en var "code".
+      //se "pushearan"(enviarán) al array vacío "txtInArrayWithAsciiCode".
       txtInArrayWithAsciiCode.push(code);
+      //Verificando que el array si fue rellenado con los valores de "code"
       console.log(txtInArrayWithAsciiCode);
     }
+    //Declarando un nuevo array que guardará con el nuevo código ASCII; encriptandolo con el
+    //offset seleccionado.
     let newArraywithEncode = txtInArrayWithAsciiCode.map((v) => {
-      console.log(v)
+      //Utilizando .map para hacer las opera. matemáticas para desplazar cada valor(v) de cada (i) 
+      //del array "txtInArrayWithAsciiCode"
+      
+      //Si (v) es mayúscula, su ASCII code entra en esta condicional.
       if (v >= 65 && v <= 90) {
         return (((v - 65) + offset) % 26) + 65;
+        
+        //Si (v) es minúscula, su ASCII code entra en esta condicional.
       } else if (v >= 97 && v <= 122) {
-        console.log((((v - 97) + offset) % 26) + 97)
         return (((v - 97) + offset) % 26) + 97;
+
+        //Si (v) es espacio vacío, su ASCII code entra en esta condicional.
       } else {
         return v = 32;
       }
-      //console.log(newArraywithEncode);
+      //Verificando que newArraywithEncode ha sido rellenado con los nuevos valores ASCII rotados.
+      console.log(newArraywithEncode);
 
     })
-     //String.fromCharCode.apply(String, newArraywithDecode);
+     
+     //Usando .apply aplico "String.fromCharCode" pero a cada elemento de nuevo array,
+     //para transformar cada código en una letra.
+     //Retorno el nuevo string ya con las posiciones rotadas.
     return String.fromCharCode.apply(String, newArraywithEncode);
+    
+    //Apliqué .join("") pero me retornó un strings de códigos juntos "65786989"
     //return newArraywithEncode.join("");
     
     
 
   },
   decode: (offset, string) => {
-    let txtInArrayWithAsciiCode2 = [];
+    //Declarando un array que contendrá los códigos ASCII
     let txTCifradoInArray2 = [];
-    let result2Decode = "";
-    let decodTxt = result2Decode;
-
+    
+    //Iniciando un loop para obtener el código ASCII de cada letra del string del usuario.
     for (let i=0; i<string.length; i++) {
-      var code2 = string.charCodeAt(i);
+      //Guardando los código ASCII en la var "code2"
+      let code2 = string.charCodeAt(i);
+      //enviando los código guardados en "code2" a txTCifradoInArray.
       txTCifradoInArray2.push(code2);
     }
-    let newArraywithDecode = txTCifradoInArray2.map((v,i,a) => {
+    //Declarando un nuevo array que contendrá los nuevos valores de cada código
+    //usando .map para realizar las ope. matemáticas de cada (v) de cada (i) del array
+    let newArraywithDecode = txTCifradoInArray2.map((v) => {
+
+      //Si el valor (v) corresponde a una mayúscula.
       if (v >= 65 && v <= 90) {
         return (((v + 65) - offset) % 26) + 65;
       }
+      //Si el valor (v) corresponde a una minúscula.
       else if (v >= 97 && v <= 122) {
         return (((v + 97) + offset) % 26) + 97;
+
+      //Si el valor (v) corresponde a espacio vacío.
       }else{
+        
         return v=32;
       }
-      //result2Decode = String.fromCharCode.apply(String, newArraywithDecode);
+      
       
     })
+     //Retorno el nuevo string ya con las posiciones rotadas.
     return String.fromCharCode.apply(String, newArraywithDecode);
   }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
+//Botón para ENCODE (CIFRAR)
 const button1 = document.getElementById("buttonEn");
+//Botón para DECODE (DESCIFRAR)
 const button2 = document.getElementById("buttonDe");
-//let string = document.getElementById("textBox1").value;
-let offset = parseInt(document.getElementById("pass1").value);
 
+
+
+//Función que conecta el evento encode con el botón 
 buttonEn.addEventListener("click", event => {
+  //Igualando el parametro "string" al valor que ingresó el usuario
   let string = document.getElementById("textBox1").value;
+
+  //Igulando el parametro "offset" al valor númerico que ingresó el usuario.
   let offset = parseInt(document.getElementById("pass1").value);
+
+  //Declarando los paramentros que usará la función "cipher.encode"
   let result2Encode = cipher.encode(offset, string);
+  //Pintando el retorno de la función "cipher.encode" en pantalla.
   document.getElementById("encodetxt").innerHTML = result2Encode;
 });
-button2.addEventListener("click", event => {
+//Función que conecta el evento decode con el botón
+buttonDe.addEventListener("click", event => {
+  //Igualando el parametro "string" al valor que ingresó el usuario.
   let string = document.getElementById("textBox2").value;
+
+  //Igulando el parametro "offset" al valor númerico que ingresó el usuario.
   let offset = parseInt(document.getElementById("pass1").value);
+
+  //Declarando los paramentros que usará la función "cipher.decode"
   let result2Decode = cipher.decode(offset, string);
+
+  //Pintando el retorno de la función "cipher.decode" en pantalla.
   document.getElementById("decodetxt").innerHTML = result2Decode;
   
 });
